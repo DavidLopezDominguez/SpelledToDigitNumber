66610235dfe612060e84ee8d695eb09a5a88f7f5
David López Domínguez <davidlopezdominguez@MacBook-Air-de-David.local>
2025-08-01 10:20:00 +0200

    codigo comentado

--- src/Presentation/UIcontroller.java
+++ src/Presentation/UIcontroller.java
@@ -9,10 +9,18 @@ public class UIcontroller {
         sc = new Scanner(System.in);
     }
 
+    /**
+     * This function shows the message received in parameters.
+     * @param message: the message to show.
+     */
     public void showMessage(String message) {
         System.out.println(message);
     }
 
+    /**
+     * This function scans a line.
+     * @return the scanned string.
+     */
     public String scanString() {
         return sc.nextLine();
     }
